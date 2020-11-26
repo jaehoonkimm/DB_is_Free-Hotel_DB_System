2b8c54db59ad5ee5f9eb539994a1e36ef9a795e3
Jinnan-Yun <yjn9709@gmail.com>
2020-11-27 04:34:59 +0900

    remove directory

--- myvenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/jinnan_yun/django_project/DB_is_Free-Hotel_DB_System/myvenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
