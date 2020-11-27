ee754ec54733d2fe9ffdde41e0653f9964e3eb69
Jinnan-Yun <yjn9709@gmail.com>
2020-11-28 02:38:18 +0900

    function : add realtime-claims form

--- function/main_app/forms.py
+++ function/main_app/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Reservation
 from .models import HouseKeepingTaskList
+from .models import RealtimeClaim
 from .models import OfficeCheckOn
 from .models import OfficeCheckOut
 
@@ -11,17 +12,8 @@ class ReservationForm(forms.ModelForm):
                 'reservation_child_count', 'reservation_group',
                 'reservation_start_date', 'reservation_end_date',
                 'reservation_breakfast_included']
-        labels = { 'reservation_online_name':'예약자 성함',
-                    'reservation_adult_count':'숙박자 수',
-                    'reservation_child_count':'동반 아동 수',
-                    'reservation_group':'소속 단체',
-                    'reservation_start_date':'체크인 예정일',
-                    'reservation_end_date':'체크아웃 예정일',
-                    'reservation_breakfast_included':'조식 포함 여부'
-        }
         widgets = {
             'reservation_start_date': forms.DateInput(format=('%m/%d/%Y'), attrs={'class':'form-control', 'placeholder':'Select a date', 'type':'date'}),
-            'reservation_end_date': forms.DateInput(format=('%m/%d/%Y'), attrs={'class':'form-control', 'placeholder':'Select a date', 'type':'date'})
         }
 
 class EmTaskAssignForm(forms.ModelForm):
@@ -39,4 +31,11 @@ class EmployeeattendanceForm(forms.ModelForm):
     class Meta:
         model = OfficeCheckOut
         fields = ['office_check_out_timestamp', 'employee'
+                ]
+
+class RealTimeClaimsForm(forms.ModelForm):
+    class Meta:
+        model = RealtimeClaim
+        fields = [ 'claim_creation_time', 'realtime_clain_content', 'claim_note',
+                'check_in', 'employee'
                 ]
