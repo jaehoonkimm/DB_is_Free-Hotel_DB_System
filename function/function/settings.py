e9de6ca01ca247d3ca98155df8167996c97b068d
jaehoonkim.dev@gmail.com <jaehoonkim.dev@gmail.com>
2020-12-07 22:08:05 +0900

    fix: add staticfiles dirs

--- function/function/settings.py
+++ function/function/settings.py
@@ -168,9 +168,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
